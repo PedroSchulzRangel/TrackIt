b6e989994c2242928ca34844150468fc459a6ded
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-03-15 21:45:03 -0300

    Requisito: Layout: tela login

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,17 @@
+import logo from '../../assets/logo.png';
+import { HomeContainer } from './StyleHome';
+import { Link } from 'react-router-dom';
+
 export default function Home(){
-    return <div>Home</div>;
+    return (<HomeContainer>
+        <img src={logo} alt="logo-trackit"/>
+        <form>
+        <input type="email" placeholder="email"/>
+        <br/>
+        <input type="text" placeholder="senha"/>
+        <br/>
+        <button type="submit">Entrar</button>
+        </form>
+        <Link to="/cadastro"><p>Não tem uma conta? Cadastre-se!</p></Link>
+    </HomeContainer>);
 }
---
+++ src/pages/home/StyleHome.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    font-family: 'Lexend Deca', sans-serif;
+    font-weight: 400;
+    font-size: 13.98px;
+    line-height: 17.47px;
+img {
+    width: 180px;
+    height: 178.38px;
+    margin-top: 68px;
+    margin-bottom: 32.62px;
+};
+input {
+    width: 303px;
+    height: 45px;
+    border: 1px solid #d4d4d4;
+    border-radius: 5px;
+    margin-bottom: 6px;
+    color: #dbdbdb;
+};
+button {
+    margin-top: 6px;
+    margin-bottom: 25px;
+    width: 303px;
+    height: 45px;
+    background-color: #52b6ff;
+    color: #FFFFFF;
+    border-radius: 4.64px;
+};
+p {
+    color: #52b6ff;
+    text-decoration: underline;
+}
+`;
