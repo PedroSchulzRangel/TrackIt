ff1b73cbaa21446a9770282254181d9818cb29b8
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-03-18 23:48:55 -0300

    Requisito: Layout tela hábitos - novo hábito

--- src/pages/habits/Habits.js
+++ src/pages/habits/Habits.js
@@ -4,18 +4,39 @@ import { AiFillPlusSquare } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
 import { TitleContainer } from './StyleHabits';
 import { Message } from './StyleHabits';
+import { HabitsContainer } from './StyleHabits';
+import { weekdaysContainer } from './StyleHabits';
+import { NewHabitContainer } from './StyleHabits';
 
 export default function Habits(){
     return (
     <IconContext.Provider value={{color: "#52B6FF", size: "50px"}}>
-    <div>
+    <HabitsContainer>
         <Header/>
         <TitleContainer>
             <h3>Meus hábitos</h3>
-            <AiFillPlusSquare/>
+            <AiFillPlusSquare data-test="habit-create-btn"/>
         </TitleContainer>
+        <NewHabitContainer>
+            <form>
+            <input type="text" placeholder="nome do hábito" required/>
+            <br/>
+            <weekdaysContainer>
+            <button type="button">D</button>
+            <button type="button">S</button>
+            <button type="button">T</button>
+            <button type="button">Q</button>
+            <button type="button">Q</button>
+            <button type="button">S</button>
+            <button type="button">S</button>
+            </weekdaysContainer>
+            <br/>
+            <button type="reset">Cancelar</button>
+            <button type="submit">Salvar</button>
+            </form>
+        </NewHabitContainer>
         <Message>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</Message>
         <Footer/>
-    </div>
+    </HabitsContainer>
     </IconContext.Provider>);
 }
--- src/pages/habits/StyleHabits.js
+++ src/pages/habits/StyleHabits.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
+export const HabitsContainer = styled.div`
+    background-color: #e5e5e5;
+    height: 100vh;
+`;
+
 export const TitleContainer = styled.div`
-    margin-top: 88px;
+    margin-top: 70px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding-left: 17px;
     padding-right: 18px;
+    padding-top: 28px;
 
     h3 {
         font-family: 'Lexend Deca', sans-serif;
@@ -25,4 +31,30 @@ export const Message = styled.p`
     color: #666666;
     margin-top: 20px;
     padding-left: 17px;
+`;
+export const NewHabitContainer = styled.div`
+    background-color: #FFFFFF;
+    width: 340px;
+    height: 180px;
+    border-radius: 5px;
+    margin-left: 17px;
+    padding-left: 19px;
+    padding-top: 18px;
+    
+    input {
+        border: 1px solid #d4d4d4;
+        width: 303px;
+        height: 45px;
+        border-radius: 5px;
+    }
+    input::placeholder{
+        font-family: 'Lexend Deca', sans-serif;
+        font-weight: 400;
+        font-size: 19.98px;
+        line-height: 24.97px;
+        color: #dbdbdb;
+    }
+`;
+export const weekdaysContainer = styled.div`
+
 `;
